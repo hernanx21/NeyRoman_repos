8a1063cb1c978b142b53d52de83ee06750349c22
hernanx21 <romanher_21@hotmail.com>
2023-04-21 23:30:58 -0500

    setting database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,24 @@
 import { Module } from '@nestjs/common';
-import { AppService } from './app.service';
-import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'cockroachdb',
+      url: process.env.DATABASE_URL,
+      ssl: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+      logging: true,
+    }),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
---
+++ utilities/repositories/respositories.utilities.ts
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+export const data = [
+    { id: 1, state: 604 },
+    { id: 2, state: 605 },
+    { id: 3, state: 606 },
+];
